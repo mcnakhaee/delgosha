85d4dc6a21779b01af09a550a8ece2c34ab8e9bc
mcnakhaee <mcnakhaee@gmail.org>
2020-08-18 22:50:40 +0200

    update description

--- R/delgosha_package.R
+++ R/delgosha_package.R
@@ -14,3 +14,16 @@
 #' @importFrom gdtools set_dummy_conf
 #' @import rmarkdown knitr htmltools
 "_PACKAGE"
+#' hrbrthemes exported operators
+#'
+#' The following functions are imported and then re-exported
+#' from the hrbrthemes package to enable use of the magrittr
+#' pipe operator with no additional library calls
+#'
+#' @name delgosha-exports
+NULL
+
+#' @name %>%
+#' @export
+#' @rdname delgosha-exports
+NULL
