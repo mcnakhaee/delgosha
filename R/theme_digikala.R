1bf6270ebd6713a926881af668a3d4f10647c725
mcnakhaee <mcnakhaee@gmail.org>
2020-08-18 15:44:53 +0200

    make geom_curve red

--- R/theme_digikala.R
+++ R/theme_digikala.R
@@ -32,7 +32,8 @@ theme_digikala <- function(base_font = "2  Yekan",
                            axis_col = "#cccccc",
                            axis = FALSE,
                            ticks = FALSE) {
-  def_fore <- '#19D9FA'
+  def_fore <- '#19D9FA' #blue
+  curve_col <- "#FB112D" #red
   text_color <- '#76787A'
   axis_line_color <- '#C9C9C9'
   axis_text_color <- '#717171'
@@ -44,14 +45,18 @@ theme_digikala <- function(base_font = "2  Yekan",
   thm <-
     thm + theme(
       text = element_text(color = text_color),
-      plot.title = element_text(family =  plot_title_font, hjust = 1,
-                                margin=margin(t =plot_title_margin,b=plot_title_margin),
-                                face = plot_title_face,
-                                color = text_color),
+      plot.title = element_text(
+        family =  plot_title_font,
+        hjust = 1,
+        margin = margin(t = plot_title_margin, b =
+                          plot_title_margin),
+        face = plot_title_face,
+        color = text_color
+      ),
       plot.subtitle = element_text(
         family =  plot_title_font,
         hjust = 1,
-        margin=margin(b=subtitle_margin),
+        margin = margin(b = subtitle_margin),
         face = subtitle_face,
         color = 'gray60'
       ),
@@ -66,24 +71,36 @@ theme_digikala <- function(base_font = "2  Yekan",
     )
 
 
-  thm <- thm +theme(axis.line=element_line(color=axis_line_color, size=0.15),
-                    panel.grid = element_blank())
+  thm <-
+    thm + theme(
+      axis.line = element_line(color = axis_line_color, size = 0.15),
+      panel.grid = element_blank()
+    )
 
-  thm <- thm + theme(strip.text = element_text(color = text_color,size = strip_text_size, margin =margin(t =strip_text_margin, b=strip_text_margin)),
-                     panel.spacing.x = unit(2, "lines"),
-                     panel.spacing.y = unit(2, "lines"),
-                     panel.spacing = plot_margin)
+  thm <-
+    thm + theme(
+      strip.text = element_text(
+        color = text_color,
+        size = strip_text_size,
+        margin = margin(t = strip_text_margin, b = strip_text_margin)
+      ),
+      panel.spacing.x = unit(2, "lines"),
+      panel.spacing.y = unit(2, "lines"),
+      panel.spacing = plot_margin
+    )
 
 
   ggplot2::update_geom_defaults("point", list(colour = def_fore))
   ggplot2::update_geom_defaults("line", list(colour = def_fore))
+  ggplot2::update_geom_defaults("curve", list(colour = curve_col))
   ggplot2::update_geom_defaults("area", list(colour = def_fore, fill = def_fore))
   ggplot2::update_geom_defaults("rect", list(colour = def_fore, fill = def_fore))
   ggplot2::update_geom_defaults("density", list(colour = def_fore, fill =
                                                   def_fore))
   ggplot2::update_geom_defaults("bar", list(colour = def_fore, fill = def_fore))
   ggplot2::update_geom_defaults("col", list(colour = def_fore, fill = def_fore))
+
   ggplot2::update_geom_defaults("text", list(colour = text_color, family = base_font))
 
-thm
+  thm
 }
