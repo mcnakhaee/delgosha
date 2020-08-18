d51baadc44e987a7c3d62f98e66cd72054866f2c
mcnakhaee <mcnakhaee@gmail.org>
2020-08-18 21:20:42 +0200

    update documentation

---
+++ R/delgosha.R
@@ -0,0 +1,18 @@
+#' Additional 'ggplot2' Themes and Theme Components designed for RTL languages and specifically for Persian
+#'
+#' A compilation of extra themes and theme components for 'ggplot2' with an
+#' emphasis on typography.
+#'
+#'
+#' @md
+#' @name delgosha
+#' @docType package
+#' @author Muhammad Chenariya Nakhaee (mcnakhaee.com)
+#' @keywords internal
+#' @import ggplot2 grid scales extrafont grDevices
+#' @importFrom magrittr %>%
+#' @importFrom gdtools set_dummy_conf
+#' @import rmarkdown knitr htmltools
+#' @importFrom tools file_path_sans_ext
+"_PACKAGE"
+
